1a6167c4580ab0701ab0da158d0229c42e7b3438
kyslynsky <kyslynsky@gmail.com>
2022-04-22 12:34:07 +0300

    Task two done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+
+const galleryBlockEl = document.querySelector(".gallery");
+
+const makeGalleryMarkup = ({ preview, original, description }) => {
+  return `
+    <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+};
+
+const makeGallery = galleryItems.map(makeGalleryMarkup).join("");
+
+galleryBlockEl.insertAdjacentHTML("afterbegin", makeGallery);
+
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  enableKeyboard: true,
+});
